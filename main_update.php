ac949fae277503d5418c9de4e5ea0a46a30ddce5
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-09 12:30:01 +0700

    cmd srvstatus

---
+++ main_update.php
@@ -0,0 +1,17 @@
+<?php 
+
+include_once("helpers/hlp_config.php");
+include_once("helpers/hlp_data.php");
+include_once("helpers/hlp_bot.php");
+
+$message_data = $update["message"];
+$chat_id = (string) $message_data["chat"]["id"];
+$message_text = $message_data["text"];
+if($message_text == "/srvstatus"){
+    $srvstatus = loadData("srvstatus");
+    KirimPerintah('sendMessage',[
+        'chat_id' => $chat_id,
+        'text' => 'srvstatus: '.print_r($srvstatus, true),
+        'disable_web_page_preview' => true,
+    ],$token);
+}
--- run.php
+++ run.php
@@ -4,4 +4,18 @@ include_once("helpers/hlp_config.php");
 include_once("helpers/hlp_data.php");
 include_once("helpers/hlp_bot.php");
 
-saveData("testRun",date("Y-m-d H:i:s"));
+
+$updates = DapatkanUpdate($update_id, $token);
+
+$bot_config = getConfig('bot', []);
+foreach($bot_config as $bot=>$config){
+    $token = $config['token'];
+    $update_id = loadData("updid_$bot",0);
+    $updates = DapatkanUpdate($update_id, $token);
+    foreach($updates as $update){
+        include("main_update.php");
+        $update_id = 1+$update["update_id"];
+    }
+    saveData("updid_$bot",$update_id);
+}
+
