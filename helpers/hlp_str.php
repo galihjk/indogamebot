9dfa227d092bfb4a04f59f2e1faf1ec7db93f561
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-28 11:21:56 +0700

    Alhamdulillah

--- helpers/hlp_str.php
+++ helpers/hlp_str.php
@@ -38,25 +38,25 @@ function isDiakhiri($string, $diakhiri, $caseSensitive = true){
 function str_compare($string_haystack, $string_needle, $type){
 	if((string)$string_haystack === "~is_not_null") return (strlen((string) $string_needle) !== 0);
 	if((string)$string_needle === "~is_not_null") return (strlen((string) $string_haystack) !== 0);
-	if(file_exists("msgcmdlog/".date("YmdHis").".txt")){
-		$resultdebug = "string_haystack=[$string_haystack], string_needle=[$string_needle], type=[$type]\n";
-		$resultdebug .= "if($type == \"exact\" and $string_haystack != \"~is_not_null\" and $string_needle != \"~is_not_null\")==>";
-		$resultdebug .= (($type == "exact" and $string_haystack != "~is_not_null" and $string_needle != "~is_not_null") ? "TRUE" : "FALSE")."\n";
-		$resultdebug .= "(string) $string_haystack === (string) $string_needle ";
-		if((string) $string_haystack === (string) $string_needle){
-			$resultdebug .= "==>>TURE\n\n";
-		}
-		else{
-			$resultdebug .= "==>>FALSE\n\n";
-		}
-		file_put_contents("msgcmdlog/".date("YmdHis").".txt",
-		$resultdebug
-		, FILE_APPEND | LOCK_EX);
-	}
+	// if(file_exists("msgcmdlog/".date("YmdHis").".txt")){
+	// 	$resultdebug = "string_haystack=[$string_haystack], string_needle=[$string_needle], type=[$type]\n";
+	// 	$resultdebug .= "if($type == \"exact\" and $string_haystack != \"~is_not_null\" and $string_needle != \"~is_not_null\")==>";
+	// 	$resultdebug .= (($type == "exact" and $string_haystack != "~is_not_null" and $string_needle != "~is_not_null") ? "TRUE" : "FALSE")."\n";
+	// 	$resultdebug .= "(string) $string_haystack === (string) $string_needle ";
+	// 	if((string) $string_haystack === (string) $string_needle){
+	// 		$resultdebug .= "==>>TURE\n\n";
+	// 	}
+	// 	else{
+	// 		$resultdebug .= "==>>FALSE\n\n";
+	// 	}
+	// 	file_put_contents("msgcmdlog/".date("YmdHis").".txt",
+	// 	$resultdebug
+	// 	, FILE_APPEND | LOCK_EX);
+	// }
 	if($type == "exact"){
-		if(file_exists("msgcmdlog/".date("YmdHis").".txt")){
-			file_put_contents("msgcmdlog/".date("YmdHis").".txt","EXACT!", FILE_APPEND | LOCK_EX);
-		}
+		// if(file_exists("msgcmdlog/".date("YmdHis").".txt")){
+		// 	file_put_contents("msgcmdlog/".date("YmdHis").".txt","EXACT!", FILE_APPEND | LOCK_EX);
+		// }
 		return ((string) $string_haystack === (string) $string_needle);
 	} 
 	if($type == "insensitive") return (strtolower($string_haystack) == strtolower($string_needle));
@@ -67,6 +67,6 @@ function str_compare($string_haystack, $string_needle, $type){
 	if($type == "last_sensitive") return isDiakhiri($string_haystack, $string_needle, true);
 	if($type == "last_insensitive") return isDiakhiri($string_haystack, $string_needle, false);
 	echo "Type ERROR: '<pre>".print_r($type,true)."</pre>'";
-	file_put_contents("msgcmdlog/str_compare".date("YmdHis").".txt","Type ERROR: '<pre>".print_r([$type,$string_haystack, $string_needle],true)."</pre>'");
+	file_put_contents("msgcmdlog/str_compare".date("YmdH").".txt","Type ERROR: '<pre>".print_r([$type,$string_haystack, $string_needle],true)."</pre>'");
 	return false;
 }
